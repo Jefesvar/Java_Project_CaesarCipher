235cfcde87b9eb8a05ddc75badea8cf7e48dddae
jefes <jefesvar@gmail.com>
2025-03-18 21:06:00 -0500

    Inicio 3.0

---
+++ .idea/src/CG/CaesarCipher/FileManager.java
@@ -0,0 +1,13 @@
+package CG.CaesarCipher;
+
+public class FileManager {
+    public String readFile(String filePath){
+        String fileText = "";
+        return fileText;
+    }
+
+    public  void  writeFile(String content, String filePath){
+
+    }
+
+}
---
+++ .idea/src/CG/CaesarCipher/Validator.java
@@ -0,0 +1,13 @@
+package CG.CaesarCipher;
+
+public class Validator {
+    public  boolean isValidKey(int key, char[] alphabet){
+        boolean validKey = false;
+        return validKey;
+    }
+
+    public boolean isFileExists(String filePath){
+        boolean fileExist = false;
+        return fileExist;
+    }
+}
