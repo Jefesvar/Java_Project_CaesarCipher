008b721abf1ad9204e3420d948f0dc90ba742537
jefes <jefesvar@gmail.com>
2025-03-18 20:31:56 -0500

    Inicio 2.0

---
+++ .idea/src/CG/CaesarCipher/Cipher.java
@@ -0,0 +1,20 @@
+package CG.CaesarCipher;
+
+public class Cipher {
+
+    private char[] alphabet;
+
+    public Cipher(char[] alphabet){
+        this.alphabet = alphabet;
+    }
+
+    public void encript(){
+
+    }
+
+    public void descript(){
+
+    }
+
+
+}
