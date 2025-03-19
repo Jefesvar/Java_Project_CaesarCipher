df0dcbf5d16e03e89ed2cf41109cb10214609440
jefes <jefesvar@gmail.com>
2025-03-19 09:51:09 -0500

    Inicio 3.0

--- .idea/src/CG/CaesarCipher/MainApp.java
+++ .idea/src/CG/CaesarCipher/MainApp.java
@@ -1,7 +1,30 @@
 package CG.CaesarCipher;
+import java.util.Scanner;
+
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class MainApp {
+
+    static PrintStream sysout = System.out;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
 
+        showMenu();
+        scanner.nextInt();
+
+
     }
+
+
+    public static void showMenu(){
+        sysout.println("Bienvenido al Cifrado Caesar");
+        sysout.println("Elije una opción (1-3):");
+        sysout.println("1. Cifrar");
+        sysout.println("2. Descifrar");
+        sysout.println("3. Salir");
+        sysout.println("Por favor, elige: ");
+    }
+
 }
